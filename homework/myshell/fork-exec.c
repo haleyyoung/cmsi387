dce2d11dec4f829f2901a2d799f6e48417cfb967
Haley Young <haleyyoung88@gmail.com>
2013-03-21 10:05:23 -0700

    Got command and arguments separated and saved

--- homework/myshell/fork-exec.c
+++ homework/myshell/fork-exec.c
@@ -10,23 +10,37 @@
 int main() {
     /* String to hold the command to run. */
     char command[256];
+    char *args[256];
+    int i;
     while (1) {
         const char *delimiter = " ";
         char *context;
         printf("Enter the command to run: ");
         fgets(command, 256, stdin);
+
         int commandLength = strlen(command);
         // Change newline to terminating 0
         command[commandLength - 1] = 0;
         printf("COMMAND %s\n", command);
-        char *argument = strtok_r(command, delimiter, &context);
-        char *realCommand = argument;
-        // Found this for loop on: http://www.gsp.com/cgi-bin/man.cgi?topic=strtok_r
-        for(argument = strtok_r(command, delimiter, &context);
+
+        char *argument; //= strtok_r(command, delimiter, &context);
+        char *realCommand; //= argument;
+
+        // Found this for-loop on: http://www.gsp.com/cgi-bin/man.cgi?topic=strtok_r
+        for(argument = strtok_r(command, delimiter, &context), i = 0;
             argument;
-            argument = strtok_r(NULL, delimiter, &context)){
-                printf("ARGUMENT %s\n", argument);
-                printf("command NOW %s\n", realCommand);
+            argument = strtok_r(NULL, delimiter, &context), i++){
+                // Save command
+                if (i == 0) {
+                    realCommand = argument;
+                    printf("command NOW %s\n", realCommand);                    
+                }
+                // Save command arguments
+                else {
+                    args[i - 1] = argument;
+                    printf("ARGUMENT , %d , %s\n", (i-1), args[i - 1]);
+                }
+                    printf("command NOW , %d, %s\n", i, realCommand);    
         }
 
         /* Variable that will store the fork result. */
@@ -45,7 +59,9 @@ int main() {
         } else {
             /* Parent process. */
             int result;
-            wait(&result);
+            if (strcmp(args[i - 1], "&") != 0) {
+                wait(&result);
+            }
             printf("All done; result = %d\n", result);
         }
 
