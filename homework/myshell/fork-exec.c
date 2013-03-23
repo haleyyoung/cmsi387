1164f47f54eb8f55256aaf48768c2a6996c22815
Haley Young <haleyyoung88@gmail.com>
2013-03-22 19:46:30 -0700

    Implemented Ctrl-D  exit from shell

--- homework/myshell/fork-exec.c
+++ homework/myshell/fork-exec.c
@@ -12,7 +12,7 @@ int main() {
     char command[256];
     char *args[256];
     int i;
-    while (1) {
+    while (!feof(stdin)) {
         const char *delimiter = " ";
         char *context;
         printf("Enter the command to run: ");
@@ -21,7 +21,7 @@ int main() {
         int commandLength = strlen(command);
         // Change newline to terminating 0
         command[commandLength - 1] = 0;
-        printf("COMMAND %s\n", command);
+   //     printf("COMMAND %s\n", command);
 
         char *argument; //= strtok_r(command, delimiter, &context);
         char *realCommand; //= argument;
@@ -33,23 +33,18 @@ int main() {
                 // Save command
                 if (i == 0) {
                     realCommand = argument;
-                    printf("command NOW %s\n", realCommand);                    
+             //       printf("command NOW %s\n", realCommand);                    
                 }
                 // Save command arguments
                 else {
                     args[i - 1] = argument;
-                    printf("ARGUMENT , %d , %s\n", (i-1), args[i - 1]);
+           //         printf("ARGUMENT , %d , %s\n", (i-1), args[i - 1]);
                 }
-                    printf("command NOW , %d, %s\n", i, realCommand);    
+         //           printf("command NOW , %d, %s\n", i, realCommand);    
         }
 
-        if (args[0]) {
-            int lastArgumentLength = strlen(args[i - 2]);
-            printf("what?!? %d, %s\n", i, args[i - 2]);
-        }
-
-            printf("command %s\n", command);
-            printf("checking %c\n", args[i-2][strlen(args[i- 2]) - 1]);
+       //     printf("command %s\n", command);
+     //       printf("checking %c\n", args[i-2][strlen(args[i- 2]) - 1]);
 
         // Check if we need to strip off the "&" from the command or last argument
         if (strcmp(&command[strlen(command) - 1], "&") == 0) {
@@ -58,7 +53,7 @@ int main() {
             args[position] = "&";
             i++;
             
-            printf("modified command %s , args %s\n", command, args[i-2]);
+   //         printf("modified command %s , args %s\n", command, args[i-2]);
         }
         else if (args[0] && (strcmp(&args[i- 2][strlen(args[i - 2]) - 1], "&") == 0)
             && (strlen(args[i - 2]) != 1)) {
@@ -66,7 +61,7 @@ int main() {
             args[i - 2][strlen(args[i- 2]) - 1] = 0;
             i++;
             
-            printf("modified args %s , args %s\n", args[i - 3], args[i-2]);
+ //           printf("modified args %s , args %s\n", args[i - 3], args[i-2]);
         }
 
         /* Variable that will store the fork result. */
@@ -85,14 +80,13 @@ int main() {
         } else {
             /* Parent process. */
             int result;
-            printf("%d\n", (strcmp(args[i - 2], "&") != 0));
+  //          printf("%d\n", (strcmp(args[i - 2], "&") != 0));
             if (args[0] && strcmp(args[i - 2], "&") != 0) {
-                printf("here\n");
+  //              printf("here\n");
                 wait(&result);
             }
             printf("All done; result = %d\n", result);
         }
-
     }
     return 0;
 }
